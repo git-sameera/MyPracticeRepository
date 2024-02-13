99c0f6e0b352e239667ce3f93d260577e38c01f4
Sameera Bhattar <sameerabhattar@gmail.com>
2024-02-13 13:45:39 +0530

    Updated TestFileGit file for testing

--- Java_Practice_Training_2_Oct_23/src/practiceJavaPackage/TestFileGit.java
+++ Java_Practice_Training_2_Oct_23/src/practiceJavaPackage/TestFileGit.java
@@ -3,7 +3,8 @@ package practiceJavaPackage;
 public class TestFileGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Sample Java Class file for Git repo training");
 
 	}
 
